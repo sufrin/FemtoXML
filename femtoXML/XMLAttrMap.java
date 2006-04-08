1a0b477e5fed46d979d1331ed0b07e6802014510
sufrin <sufrin@6554113d-4c10-0410-8e1f-cd3aaf3e9675>
2006-04-08 22:01:12 +0000

    Keywords added
    git-svn-id: svn+ssh://linux.cs.ox.ac.uk/users/sufrin/SVN/FemtoXML@19 6554113d-4c10-0410-8e1f-cd3aaf3e9675

---
+++ femtoXML/XMLAttrMap.java
@@ -0,0 +1,73 @@
+package femtoXML;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+/** 
+ * An implementation of <code>XMLAttributes</code> that prints the attributes of entitities
+ * in re-readable form.
+ */
+@SuppressWarnings("serial")
+public class XMLAttrMap extends LinkedHashMap<String, String> implements XMLAttributes
+{ /** True if entitities were expanded in attribute values when they were read.*/
+  protected boolean expandedEntities;
+  
+  /** Largest number of key=value pairs before this is split across several lines when printed */
+  protected int split = 2;
+  
+  /** Set the largest number of key=value pairs before this will be split across several lines when printed  */
+  public XMLAttributes setSplit(int split) { this.split = split; return this; }
+  
+  /** Current largest number of key=value pairs before this will be split across several lines when printed  */
+  public int getSplit() { return split; }
+  
+  /** 
+   * 
+   * @param expandEntities -- true if entities were expanded within values. 
+   */
+  public XMLAttrMap(boolean expandEntities) 
+  { 
+    this.expandedEntities = expandEntities; 
+  }
+  
+  /** this(true) */
+  public XMLAttrMap()
+  {
+    this(true);
+  }
+  
+  public String toString()
+  {
+    StringWriter sw  = new StringWriter();
+    PrintWriter  out = new PrintWriter(sw);
+    printTo(out, 0);
+    out.flush();
+    return sw.toString();
+  }
+  
+  /** Print in re-readable form on the given PrintWriter, using the given indent if 
+   *  the printed form is split across lines.
+   */
+  public void printTo(PrintWriter out, int indent)
+  {  Set<String> keys = keySet();
+     boolean indenting = keys.size()>split && indent>0;
+     for (String key: keys)
+     { String val = get(key);
+       char quote = val.indexOf('"')==-1 ? '"' : '\'';
+       if (indenting) 
+       {  out.println();
+          for (int i=0; i<indent; i++) out.print(" ");
+       }
+       else 
+          out.print(" ");
+       out.print(key);
+       out.print("=");
+       out.print(quote);
+       if (expandedEntities) XMLCharUtil.print(out, val); else out.print(val);
+       out.print(quote);
+     }
+  }
+
+}
