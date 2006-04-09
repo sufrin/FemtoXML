b418719261b91409b216fef7bb1488c7dfefd8c8
sufrin <sufrin@6554113d-4c10-0410-8e1f-cd3aaf3e9675>
2006-04-09 21:30:21 +0000

    git-svn-id: svn+ssh://linux.cs.ox.ac.uk/users/sufrin/SVN/FemtoXML@24 6554113d-4c10-0410-8e1f-cd3aaf3e9675

---
+++ femtoXML/app/AppDOCTYPE.java
@@ -0,0 +1,37 @@
+package femtoXML.app;
+
+import femtoXML.FormatWriter;
+
+public class AppDOCTYPE implements AppTree
+{
+  protected String text;
+
+  public AppDOCTYPE(String text)
+  {
+    this.text = text;
+  }
+  
+  
+  /** Generates the human-readable form of the word text. */
+  public String toString()
+  {
+    return text;
+  }
+  
+  /** Outputs the text of this PI. 
+   *  
+   */
+  public void printTo(FormatWriter out, int indent)
+  {
+      out.indent(indent);
+      out.print("<!DOCTYPE ");
+      out.print(text);
+      out.println(">");
+  }
+
+
+  public boolean isWord()
+  {
+    return false;
+  }
+}
